a42da35fde21e3dcd81056d59d1edc208cfe6c04
chancet1982 <chancet1982@gmail.com>
2015-11-05 13:39:53 +0100

    changed dismiss plugin to work on an auto generated ID based on alertbox content.
    this allows it to control multiple alertboxes. and resets the dismissale
    in case the content of the box was updated.

--- jquery.dismiss.js
+++ jquery.dismiss.js
@@ -9,19 +9,39 @@
         }, options );
  
  		var dis = function ($thisObj) {
-			if (typeof(Storage) !== "undefined" && $thisObj.attr("id") &&  !$thisObj.hasClass("dismissed")) {
-				localStorage.setItem($thisObj.attr("id"), "dismissed");
+			if (typeof(Storage) !== "undefined" && !$thisObj.hasClass("dismissed")) {
+				localStorage.setItem(genId($thisObj), "dismissed");
 				$thisObj.addClass("dismissed").hide();
 			}
 		};
+
+ 		var genId = function ($thisObj) {
+        console.log("genId called");
+ 			id = 0, id_length = 0, des_length = 8;
+ 			s = $thisObj.text();
+			for (var i = 0; i < s.length; i++){
+			    id = id + s.charCodeAt(i);
+			}
+            id_length = (Math.log10((id ^ (id >> 31)) - (id >> 31)) | 0) + 1;
+            
+            if (id_length < des_length) {
+            	id = id + 10000000;
+			} else if (id_length > des_length) {
+				access = id_length - des_length;
+				id.slice(0, -access);
+			}
+            
+			console.log("final id is: id" + id);
+			return "id"+id;
+		};		
 
         // in the begnining we add a class dismissed to each of the elements
         $(this).each(function(){
         	var $thisObj = $(this);
         	$thisObj.addClass("dismissable");
-			
+        	id = genId($thisObj);
 			// dismissing already dismissed objects
-            if (typeof(Storage) !== "undefined" && localStorage.getItem($thisObj.attr("id")) == "dismissed")
+            if (typeof(Storage) !== "undefined" && localStorage.getItem(genId($thisObj)) == "dismissed")
              	dis($thisObj);
 
 			// auto dismiss should hide after a certain period
